bd83ae4ee24cfd14c2a09910f6f283a533005a9e
incihuseynli <inci.huseynli0@gmail.com>
2023-11-27 08:27:22 +0400

    Data fetched and Reveal animation added

--- src/components/AboutSectComponents/AboutSect.jsx
+++ src/components/AboutSectComponents/AboutSect.jsx
@@ -1,48 +1,61 @@
-import React from "react";
 import BreadCrumbs from "../HeaderComponents/BreadCrumbs";
 import Btn from "../Buttons/Btn.jsx";
-
+import RevealAnimation from "../RevealAnimation.jsx";
+import { useEffect, useState } from "react";
+import Axios from "axios";
 const AboutSect = () => {
+  const URL =
+    "https://my-json-server.typicode.com/dbForProjects/OtherDatas/about";
+  const [about, setAbout] = useState([]);
+  useEffect(() => {
+    Axios.get(URL).then(({ data }) => {
+      setAbout(data);
+    });
+  }, []);
   return (
-    <section
-      className="
-      w-full my-24 px-4 py-4 md:px-8 lg:px-16 grid grid-cols-1 place-items-center text-center font-poppins
-      lg:grid-cols-2 lg:justify-between lg:items-start lg:gap-20 gap-8
-      "
-    >
-      <div className="flex flex-col gap-8 text-left ">
-        <BreadCrumbs menuPath="/haqqımızda" menuTitle="Haqqımızda" />
-        <h4
-          className="
-        text-3xl font-bold text-secondary-black tracking-wider
-        "
-        >
-          Sizə verdiyimiz dəyəri hiss edin
-        </h4>
-        <img
-          src="./about1.jpg"
-          alt="Layihə"
-          className="w-[640px] rounded-xl mt-10"
-        />
-      </div>
-      {/* ============================ xxxxxxxx ================================= */}
-      <div className="flex flex-col gap-6 items-center text-center lg:items-start lg:text-left">
-        <img src="./about2.jpg" alt="Layihə" className="w-[640px] rounded-xl" />
-        <p
-          className="
-            w-3/4  lg:w-full
-            text-lg font-normal tracking-wide text-primary-blue-900"
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum cum
-          voluptate debitis dolor ducimus quasi.Lorem ipsum dolor sit amet
-          consectetur adipisicing elit.
-        </p>
-        <p className="text-lg -mt-4 font-normal tracking-wide text-primary-blue-900">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
-        <Btn btnPath="/haqqımızda" btnText="Daha çox" />
-      </div>
-    </section>
+    <>
+      {about.map(({ id, pageTitle, img1, img2, caption, title }) => {
+        return (
+          <section
+            key={id}
+            className="w-full my-24 px-4 py-4 md:px-8 lg:px-16 grid grid-cols-1 place-items-center text-center font-poppins
+            lg:grid-cols-2 lg:justify-between lg:items-start lg:gap-20 gap-8"
+          >
+            <div className="flex flex-col gap-8 text-left ">
+              <RevealAnimation>
+                <BreadCrumbs menuTitle={pageTitle} />
+              </RevealAnimation>
+              <RevealAnimation>
+                <h4 className="text-3xl font-bold text-secondary-black tracking-wider">
+                  {title}
+                </h4>
+              </RevealAnimation>
+              <RevealAnimation>
+                <img
+                  src={img1}
+                  alt="Layihə"
+                  className="w-[640px] rounded-xl mt-10"
+                />
+              </RevealAnimation>
+            </div>
+
+            {/* ============================ xxxxxxxx ================================= */}
+            <div className="flex flex-col gap-6 items-center text-center lg:items-start lg:text-left">
+              <RevealAnimation>
+                <img src={img2} alt="Layihə" className="w-[640px] rounded-xl" />
+              </RevealAnimation>
+              <RevealAnimation>
+                <p className=" lg:w-full text-lg font-normal tracking-wide text-primary-blue-900">
+                  {caption}
+                </p>
+              </RevealAnimation>
+
+              <Btn btnPath="/haqqımızda" btnText="Daha çox" />
+            </div>
+          </section>
+        );
+      })}
+    </>
   );
 };
 
