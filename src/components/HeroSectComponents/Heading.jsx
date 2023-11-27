528799e38d991bfd0dd7c5967294399e3f4aca64
incihuseynli <inci.huseynli0@gmail.com>
2023-11-27 08:33:35 +0400

    Heading component edited

--- src/components/HeroSectComponents/Heading.jsx
+++ src/components/HeroSectComponents/Heading.jsx
@@ -1,16 +1,12 @@
-import React from "react";
 import Btn from "../Buttons/Btn";
 
 const Heading = () => {
   return (
-    <div className="flex flex-col gap-6 md:w-4/6 lg:w-full">
-      <h1
-        className="text-6xl font-bold tracking-wide
-       text-primary-blue-800 text-left"
-      >
+    <div className="flex flex-col gap-6 md:w-4/6 lg:w-full items-center md:items-start">
+      <h1 className="text-5xl md:text-6xl font-bold tracking-wide text-primary-blue-800 text-center md:text-left">
         Yaşamağı sevəcəyiniz yeri kəşf edin!
       </h1>
-      <p className="text-primary-blue-800 text-left font-extralight text-lg">
+      <p className="text-primary-blue-800 text-center md:text-left font-extralight text-lg">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
         officiis necessitatibus eligendi error reprehenderit nam consequuntur,
         totam labore autem? Tenetur!
