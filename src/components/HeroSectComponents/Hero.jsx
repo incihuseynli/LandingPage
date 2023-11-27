65f4d7cc965b705595fa736dd8bd553526569203
incihuseynli <inci.huseynli0@gmail.com>
2023-11-27 08:34:46 +0400

    Hero component edited and Reveal animation added

--- src/components/HeroSectComponents/Hero.jsx
+++ src/components/HeroSectComponents/Hero.jsx
@@ -1,22 +1,32 @@
-import React from "react";
 import Heading from "./Heading";
 import HeroSlider from "../Sliders/HeroSlider.jsx";
 import Stats from "./Stats.jsx";
+import RevealAnimation from "../RevealAnimation.jsx";
+
 const Hero = () => {
+  
   return (
     <section>
       <div
-        className="grid grid-cols-1 px-4 py-8 
+        className="grid grid-cols-1 px-4 py-8 md:px-8 gap-8
         lg:grid-cols-[34rem_1fr] lg:px-16
         "
       >
         {/* ================ Heading =============== */}
-        <Heading />
+        <RevealAnimation>
+          <Heading />
+        </RevealAnimation>
+
         {/* ================ Slider =============== */}
-        <HeroSlider />
+
+        <RevealAnimation>
+          <HeroSlider />
+        </RevealAnimation>
       </div>
       {/* ================ Stats =============== */}
-      <Stats/>
+      <RevealAnimation>
+        <Stats />
+      </RevealAnimation>
     </section>
   );
 };
